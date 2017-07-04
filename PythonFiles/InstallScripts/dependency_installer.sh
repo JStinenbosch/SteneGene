0071e6070669318c56f520763f8e0601d817c5c5
FrankTN <f.g.te.nijenhuis@student.rug.nl>
2017-07-04 14:07:36 +0200

    Wrote first verion of install script

---
+++ PythonFiles/InstallScripts/dependency_installer.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+installBlast+(){
+if which blastn > /dev/null; then
+    echo Blast is already installed
+else
+    sudo apt-get install ncbi-blast+
+
+#    wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.6.0+-x64-linux.tar.gz
+#    tar xzvf ncbi-blast-2.6.0+-x64-linux.tar.gz
+#    ./configure && make && sudo make install
+fi
+}
+installBedtools(){
+if which bedtools > /dev/null; then
+    echo Bedtools are already installed
+else
+    sudo apt-get install bedtools
+fi
+}
+
+installClustalO(){
+if which clustalo > /dev/null; then
+    echo ClustalO is already installed
+else
+    sudo apt-get install clustalo
+fi
+}
+
+installORFfinder(){
+if [ -f ORFfinder ]; then
+ echo ORFfinder is already available locally
+else
+ wget ftp://ftp.ncbi.nlm.nih.gov/genomes/TOOLS/ORFfinder/linux-i64/ORFfinder.gz
+ gunzip ORFfinder.gz
+ chmod +x ORFfinder
+fi
+}
+
+installBlast+
+installBedtools
+installClustalO
+installORFfinder
---
+++ PythonFiles/InstallScripts/installer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install all external dependencies
+./dependency_installer.sh
+
+# Install virtualEnv
+./virtual_env_setup.sh
